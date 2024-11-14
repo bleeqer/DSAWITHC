3254eb8fe33c5c957f66a6e0388f0e11aa15eaae
bleeqer <bleeqtheproducer@gmail.com>
2024-11-14 15:00:21 +0900

    -

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+char arr[51], answer[51];
+int len;
+
+int main()
+{
+    scanf("%s", arr);
+    len = (int) strlen(arr);
+
+    for (int i = 0; i < len; i++)
+    {
+        answer[i] = 'a' + 26;
+    }
+
+    for (int i = 0; i < len - )
+}
