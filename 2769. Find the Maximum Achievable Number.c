7497081f92c2bce70dbe84fe0384b47e9cbab603
bleeqer <59035549+bleeqer@users.noreply.github.com>
2024-11-14 17:17:54 +0900

    2769. Find the Maximum Achievable Number

---
+++ 2769. Find the Maximum Achievable Number.c
@@ -0,0 +1,7 @@
+int theMaximumAchievableX(int num, int t) 
+{
+    // num에 t를 두 배한 값을 더하여 최대 가능한 값을 계산하는 함수
+    // num: 기본 숫자
+    // t: 두 배로 늘릴 횟수
+    return num + t * 2; // num에 t * 2를 더하여 최대값을 반환
+}
