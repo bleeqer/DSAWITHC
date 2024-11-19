6e09fa4d8ff5846aa16a76da1c7266335acf5826
bleeqer <59035549+bleeqer@users.noreply.github.com>
2024-11-20 07:46:11 +0900

    136. Single Number

--- 136. Single Number.cpp
+++ 136. Single Number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
+        int result = 0; // 결과를 저장할 변수 초기화
 
+        // 배열의 모든 숫자에 대해 XOR 연산 수행
         for (int num : nums) {
-            result ^= num;
+            result ^= num; // 현재 숫자와 결과값을 XOR
         }
 
-        return result;
+        return result; // 단일 숫자 반환
     }
 };
